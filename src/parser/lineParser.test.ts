4cb4be37e96910c8981bc5bcaf732bbe11d21f3c
Konrad Zapala <konrad.zapala@avanade.com>
2020-05-29 22:43:48 +0200

    Added logic for parser to Material and Warehouse.

--- src/parser/lineParser.test.ts
+++ src/parser/lineParser.test.ts
@@ -1,4 +1,6 @@
 import LineParser from "./lineParser";
+import { Material } from "../domain/material";
+import { Warehouse } from "../domain/warehouse";
 
 describe('Given line parser', () => {
     it('should return line without modification', () => {
@@ -13,9 +15,29 @@ describe('Given line parser', () => {
         expect(lineParser.getLine()).toBe("");
     });
 
-    it('should return line empty, when contains white spaces', () => {
+    it('should return line empty, when contains white spaces and ignored sign at the beginning', () => {
         const inputString = "   #   Test123"
         const lineParser = new LineParser(inputString);
         expect(lineParser.getLine()).toBe("");
+    });
+
+    it('should return material object, when is in correct format', () => {
+        const inputString = "CHerry Hardwood Archer Door - PS;COM100001;WH-A,5|WH-B,10"
+        const lineParser = new LineParser(inputString);
+        const expectedMaterial : Material = {
+            name : "CHerry Hardwood Archer Door - PS",
+            id : "COM100001"
+        };
+        expect(lineParser.getMaterial()).toStrictEqual(expectedMaterial);
+    });
+
+    it('should return wareshouse object with total materials, when is in correct format', () => {
+        const inputString = "CHerry Hardwood Archer Door - PS;COM100001;WH-A,5|WH-B,10"
+        const lineParser = new LineParser(inputString);
+        const expectedWarhouse : Array<Warehouse> = [
+            {name: "WH-A", totalMaterials: 5 },
+            {name: "WH-B", totalMaterials: 10 }
+        ]
+        expect(lineParser.getWarehouses()).toEqual(expectedWarhouse);
     });
 });
--- src/parser/lineParser.ts
+++ src/parser/lineParser.ts
@@ -1,11 +1,28 @@
+import { Material } from "../domain/material";
+import { Warehouse } from "../domain/warehouse";
+
 class LineParser {
     readonly line : string; 
     constructor(inputLine : string) {
         this.line = this.formatLine(inputLine);
     }
 
-    public getLine() {
+    public getLine() : string {
         return this.line;
+    }
+
+    public getMaterial(materialSeparator = ";") : Material {
+        const splittedLine = this.line.split(materialSeparator);
+        return { id : splittedLine[1], name : splittedLine[0] };
+    }
+
+    public getWarehouses(warehouseSeparator = "|", warehouseAvailabilitySeparator = ",") : Array<Warehouse> {
+        const splittedLine = this.line.split(";"); //TODO merge with material one
+        const warehouses = splittedLine[2].split(warehouseSeparator);
+        return warehouses.map(warehouse => {
+            let warehouseDetails = warehouse.split(warehouseAvailabilitySeparator);
+            return {name: warehouseDetails[0], totalMaterials: parseInt(warehouseDetails[1]) };
+        });
     }
 
     private formatLine(inputLine : string) : string {
